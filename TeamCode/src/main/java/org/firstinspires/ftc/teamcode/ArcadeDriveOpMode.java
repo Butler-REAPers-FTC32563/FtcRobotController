41b94133b1cb8bf4b9ea7cdd23ed19f59cab9d2f
Butler REAPers FTC32563 <butlerreaper@gmail.com>
2026-05-19 18:36:39 -0400

    PUSH TO PROD NO TESTING

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDriveOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ArcadeTankDrive;
+
+public class ArcadeDriveOpMode extends OpMode {
+
+    ArcadeTankDrive drive = new ArcadeTankDrive();
+    double throttle, spin;
+
+    @Override
+    public void init()  {
+    drive.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop(){
+    throttle = -gamepad1.left_stick_y;
+    spin = gamepad1.left_stick_x;
+
+    drive.drive(throttle, spin);
+
+    }
+}
