dd9b0e0fb292cc0dc7f95a360ac9f737a0e197ce
LofiDev47 <thedragonrider46@gmail.com>
2026-08-27 18:43:38 -0400

    added toggleable intake with gamepad a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotOrientedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotOrientedOpMode.java
@@ -26,7 +26,7 @@ public class MecanumRobotOrientedOpMode extends OpMode {
         strafe = gamepad1.left_stick_x;
         rotate = gamepad1.right_stick_x;
 
-        if(gamepad1.a) {
+        if(gamepad1.aWasPressed()) {
             if(intakeOn = false) {
                 intakeOn = true;
             }
